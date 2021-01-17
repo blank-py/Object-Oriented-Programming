de661ad978fdcec4bfe0aaeb270d5ffaa79e68fa
blankdev <jesse.malinen1@edu.turkuamk.fi>
2021-01-17 13:30:51 +0200

    changed integers.py into functions

--- integers.py
+++ integers.py
@@ -5,20 +5,29 @@
 
 numList = []
 
+def integreader():
+    while True:
+        num = int(input("Input an integer (0 terminates):"))
+        if num == 0:
+            break
+        else:
+            numList.append(num)
 
-while True:
-    num = int(input("Input an integer (0 terminates):"))
-    if num == 0:
-        break
-    else:
-        numList.append(num)
 
+    print("Terminated")
+    print("Negative integers in the list: ", (sum(1 for num in numList if num <0)))
 
-print("Terminated")
+def evenreader():
+    even_sum = 0
+    even_sum = sum(1 for num in numList if num % 2 == 0)
+    print("Even integers in the list: ", (even_sum))
 
-div_sum = 0
-div_sum = sum(num for num in numList if num > 0 and num % 3 == 0)
+def divreader():
+    div_sum = 0
+    div_sum = sum(num for num in numList if num > 0 and num % 3 == 0)
 
-print("Negative integers in the list: ", (sum(1 for num in numList if num <0)))
-print("Even integers in the list: ", (sum(1 for num in numList if num % 2 == 0)))
-print("Sum of positive integers divisible by 3: ", (div_sum))
+    print("Sum of positive integers divisible by 3: ", (div_sum))
+
+integreader()
+evenreader()
+divreader()

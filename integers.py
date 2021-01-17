8c259841ea6a2c30b843b228275d84142457ae78
blankdev <jesse.malinen1@edu.turkuamk.fi>
2021-01-17 13:05:39 +0200

    added div_sum

--- integers.py
+++ integers.py
@@ -6,10 +6,7 @@
 numList = []
 
 
-num = input("input an integer (0 stops):")
-
-
-while num !=0:
+while True:
     num = int(input("Input an integer (0 terminates):"))
     if num == 0:
         break
@@ -19,8 +16,9 @@ while num !=0:
 
 print("Terminated")
 
+div_sum = 0
+div_sum = sum(num for num in numList if num > 0 and num % 3 == 0)
 
 print("Negative integers in the list: ", (sum(1 for num in numList if num <0)))
-
-
 print("Even integers in the list: ", (sum(1 for num in numList if num % 2 == 0)))
+print("Sum of positive integers divisible by 3: ", (div_sum))
